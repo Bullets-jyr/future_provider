4fda6c13f91509c7c99ccf02e6327546c5df1df8
Bullets-jyr <jyr@bullets.co.kr>
2024-01-11 12:28:19 +0900

    27. FutureProvider - userListProvider, UserListPage

---
+++ lib/pages/users/users_providers.dart
@@ -0,0 +1,51 @@
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../models/user.dart';
+import '../../providers/dio_provider.dart';
+
+part 'users_providers.g.dart';
+
+// final userListProvider = FutureProvider<List<User>>((ref) async {
+// final userListProvider = FutureProvider.autoDispose<List<User>>((ref) async {
+//   ref.onDispose(() {
+//     print('[userListProvider] disposed');
+//   });
+//   final response = await ref.watch(dioProvider).get('/users');
+//   // throw 'Fail to fetch user list';
+//   final List userList = response.data;
+//   final users = [for (final user in userList) User.fromJson(user)];
+//   return users;
+// });
+
+@riverpod
+FutureOr<List<User>> userList(UserListRef ref) async {
+  ref.onDispose(() {
+    print('[userListProvider] disposed');
+  });
+  final response = await ref.watch(dioProvider).get('/users');
+  // throw 'Fail to fetch user list';
+  final List userList = response.data;
+  final users = [for (final user in userList) User.fromJson(user)];
+  return users;
+}
+
+// final userDetailProvider =
+//     FutureProvider.autoDispose.family<User, int>((ref, id) async {
+//   ref.onDispose(() {
+//     print('[userDetailProvider($id)] disposed');
+//   });
+//   final response = await ref.watch(dioProvider).get('/users/$id');
+//   final user = User.fromJson(response.data);
+//   return user;
+// });
+
+@riverpod
+FutureOr<User> userDetail(UserDetailRef ref, int id) async {
+  ref.onDispose(() {
+    print('[userDetailProvider($id)] disposed');
+  });
+  final response = await ref.watch(dioProvider).get('/users/$id');
+  final user = User.fromJson(response.data);
+  return user;
+}
